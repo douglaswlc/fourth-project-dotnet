fef76d45a24de4f85c1c39f3da466669ca96dbf6
Douglas <douglaswlc@gmail.com>
2024-07-03 08:56:15 -0300

    calculator

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using fourth_project_dotnet.Models;
+
+Calculadora calc = new Calculadora();
+
+// calc.Somar(33, 17);
+// calc.Subtrair(10, 5);
+// calc.Dividir(25, 5);
+// calc.Multiplicar(5, 8);
+
+calc.Potencia(3,3);
